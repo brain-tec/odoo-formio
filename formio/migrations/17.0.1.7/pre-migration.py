43f54c22b06cdcdf9d50ba0f816a76be63acde30
bobslee <bob@novacode.nl>
2025-04-08 18:41:16 +0000

    [IMP] formio: SQL Migrations ALTER TABLE ADD COLUMN with IF NOT EXISTS check

--- formio/migrations/17.0.1.7/pre-migration.py
+++ formio/migrations/17.0.1.7/pre-migration.py
@@ -2,9 +2,9 @@
 # See LICENSE file for full licensing details.
 
 def migrate(cr, version):
-    cr.execute('ALTER TABLE formio_builder ADD COLUMN backend_submission_url_add_query_params_from character varying')
-    cr.execute('ALTER TABLE formio_builder ADD COLUMN portal_submission_url_add_query_params_from character varying')
-    cr.execute('ALTER TABLE formio_builder ADD COLUMN public_submission_url_add_query_params_from character varying')
+    cr.execute('ALTER TABLE formio_builder ADD COLUMN IF NOT EXISTS backend_submission_url_add_query_params_from character varying')
+    cr.execute('ALTER TABLE formio_builder ADD COLUMN IF NOT EXISTS portal_submission_url_add_query_params_from character varying')
+    cr.execute('ALTER TABLE formio_builder ADD COLUMN IF NOT EXISTS public_submission_url_add_query_params_from character varying')
     cr.execute('UPDATE formio_builder SET backend_submission_url_add_query_params_from = submission_url_add_query_params_from')
     cr.execute('UPDATE formio_builder SET portal_submission_url_add_query_params_from = submission_url_add_query_params_from')
     cr.execute('UPDATE formio_builder SET public_submission_url_add_query_params_from = submission_url_add_query_params_from')
